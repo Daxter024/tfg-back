a061b9d594f81fc84fcacd0a9063b481bb505f92
fran <francanadas1@gmail.com>
2025-11-13 17:04:55 +0100

    Initial set up

---
+++ src/test/java/com/agro/agritrack/AgritrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agro.agritrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgritrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
