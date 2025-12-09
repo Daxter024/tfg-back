9955410ddcf444e7729f265ff78d2b433bde40c7
fran <francanadas1@gmail.com>
2025-12-09 12:28:54 +0100

    feat('auth-service'): add validation to dto requests

---
+++ auth-service/src/main/java/com/agro/authservice/exception/GlobalExceptionHandler.java
@@ -0,0 +1,34 @@
+package com.agro.authservice.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ProblemDetail> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+
+        List<String> errors = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(error -> error.getField() + ": " + error.getDefaultMessage())
+                .collect(Collectors.toList());
+
+        ProblemDetail problemDetail = ProblemDetail.forStatus(status);
+        problemDetail.setTitle("Campos invalido");
+        problemDetail.setProperty("errors", errors);
+        return ResponseEntity
+                .status(status)
+                .body(problemDetail);
+    }
+    
+}
