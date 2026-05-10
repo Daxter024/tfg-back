245ab1b8a5cdb8c8f92a8891d9cb7cd9c29da55e
francanadas1 <francanadas1@gmail.com>
2026-05-10 14:43:09 +0200

    refactor(season-service): Season uses LocalDate instead of legacy Date

--- season-service/src/main/java/com/agro/seasonservice/model/Season.java
+++ season-service/src/main/java/com/agro/seasonservice/model/Season.java
@@ -1,18 +1,14 @@
 package com.agro.seasonservice.model;
 
-import java.util.Date;
+import java.time.LocalDate;
 import java.util.UUID;
-
-/*
-Add Jakarta in the future
- */
 
 public record Season(
         UUID id,
         UUID terrain_id,
         UUID crop_id,
-        Date start_date,
-        Date end_date,
+        LocalDate start_date,
+        LocalDate end_date,
         String observations,
         Integer season_type_id
 ) {
